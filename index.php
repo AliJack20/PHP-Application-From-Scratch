b354759882f75c665a67fa740abde2fe1f28d8a0
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-06-14 19:52:42 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="wodth=device-width, initial-scale=1.0">
+    <div class="card">
+        <div class="card-header">
+            <h3>PHP PDO CRUD 
+                <a href="product-add.php" class="btn btn-primary">Add Product</a>
+            </h3>
+        </div>
+    </div>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
---
+++ product-add.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="wodth=device-width, initial-scale=1.0">
+    <title>Insert data into databse using PHP PDO</title>
+    <div class="card">
+        <div class="card-header">
+            <h3>Insert data into databse using PHP PDO
+                <a href="index.php" class="btn btn-danger">BACK</a>
+            </h3>
+        </div>
+        <div class="card-body">
+    </div>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
